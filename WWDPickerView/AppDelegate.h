6f51c2bb2dbdf1e62d2e081e87ed022e7ac647d9
maginawin <maginawin@maginawindeMac-mini.local>
2015-02-03 15:20:43 +0800

    Initial Commit

---
+++ WWDPickerView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WWDPickerView
+//
+//  Created by maginawin on 15-2-3.
+//  Copyright (c) 2015年 mycj.wwd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WWDPickerView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WWDPickerView
+//
+//  Created by maginawin on 15-2-3.
+//  Copyright (c) 2015年 mycj.wwd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
